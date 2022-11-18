44bfae99d8b3a9966af8ea35cf0063f3f24390bf
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2022-11-18 14:11:28 +0000

    [Update]１０章_投稿機能の作成（３）-投稿できるようにしよう_202211182311

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,17 +1,18 @@
-class PostimagesController < ApplicationController
+class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
 
-#投稿データの保存
-def create
+  #投稿データの保存
+  def create
   @post_image = PostImage.new(post_image_params)
   @post_image.user_id = current_user.id
   @post_image.save
   redirect_to post_images_path
-end
+  end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
@@ -22,7 +23,7 @@ end
 
   #投稿データのストロングパラメータ
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
